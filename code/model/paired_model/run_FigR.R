c5b0ad8bfc30571a9dba487383617bab53770ce8
wuwei77lx <115720355+wuwei77lx@users.noreply.github.com>
2025-06-10 20:33:56 +0800

    Update run_FigR.R

--- code/model/paired_model/run_FigR.R
+++ code/model/paired_model/run_FigR.R
@@ -1,48 +1,53 @@
 rm(list=ls())
-# load the required libraries
+
+# Load required libraries
 library(Seurat)
 library(Signac)
 library(FigR)
 
-#  load data
-pbmc<-readRDS("pbmc_multiomic.rds")
+# Load multiomic PBMC dataset
+pbmc <- readRDS("PBMC_multiomic.rds")
 
-# construct the ATAC data format for FigR
-SEfromSignac <- function(obj, # Seurat object containing assay that houses peak (ATAC) info
-                         assayName="peaks", # Assay name for peak (ATAC) info
-                         fetchRawCounts=TRUE){ # Whether to use raw counts (Default), otherwise fetch normalized counts if present 
-  message("Pulling info from assay container: ",assayName,"\n")
+# Function to convert Seurat ATAC assay to SummarizedExperiment for FigR
+SEfromSignac <- function(obj, assayName="peaks", fetchRawCounts=TRUE) {
+  message("Pulling info from assay container: ", assayName, "\n")
   
   peakRanges <- obj@assays[[assayName]]@ranges
   
-  if(fetchRawCounts){
+  if (fetchRawCounts) {
     message("Using raw peak counts to store in SE ..\n")
     peakCounts <- obj@assays[[assayName]]@counts
   } else {
-    message("WARNING: Pulling from normalized peak count slot.\n If running FigR's gene-peak association testing, make sure you set normalizeATACmat to FALSE in the runGenePeakcorr and getDORCscores functions to avoid renormalizing data internally")
+    message("WARNING: Using normalized peak counts. Set normalizeATACmat=FALSE when running FigR functions to avoid re-normalization.\n")
     peakCounts <- obj@assays[[assayName]]@data
   }
   
-  
   cellMeta <- DataFrame(obj@meta.data)
   
+  SE <- SummarizedExperiment::SummarizedExperiment(
+    assays = list(counts = peakCounts),
+    rowRanges = peakRanges,
+    colData = cellMeta
+  )
   
-  SE <- SummarizedExperiment::SummarizedExperiment(assays = list(counts=peakCounts),
-                                                   rowRanges = peakRanges,
-                                                   colData = cellMeta)
   return(SE)
 }
-ATAC.se <- SEfromSignac(obj=pbmc,assayName = "peaks",fetchRawCounts = TRUE)
 
-# run using multiple cores if parallel support,predict peak-gene
-cisCor <- runGenePeakcorr(ATAC.se,
-                          RNAmat = pbmc@assays[["SCT"]]@data, #if Seurat V5 ,RNAmat = object@assays[[rna_assay]]@layers[["counts"]]
-                          windowPadSize=250000,
-                          genome = "hg38", # Also supports mm10 and hg38
-                          nCores = 48,
-                          p.cut=NULL)
+# Convert ATAC assay to SummarizedExperiment
+ATAC.se <- SEfromSignac(obj = pbmc, assayName = "peaks", fetchRawCounts = TRUE)
 
-# filter peak-gene correlations by p-value (select)
-cisCor.filt <- cisCor %>% filter(pvalZ <= 0.05)
+# Run peak-gene correlation analysis using FigR (supports parallelization)
+cisCor <- runGenePeakcorr(
+  ATAC.se,
+  RNAmat = pbmc@assays[["SCT"]]@data,  # RNA assay matrix (normalized SCT counts)
+  windowPadSize = 250000,               # +/- 250 kb window around genes
+  genome = "hg38",                     # Genome build (hg38 or mm10)
+  nCores = 48,                        # Number of CPU cores to use
+  p.cut = NULL                        # No p-value cutoff here; will filter later
+)
 
-save(cisCor,file="FigR_pbmc_peak_gene.rda")
+# Filter significant correlations by p-value (select)
+cisCor.filt <- cisCor %>% dplyr::filter(pvalZ <= 0.05)
+
+# Save results
+save(cisCor, file = "FigR_PBMC_peak_gene.rda")
